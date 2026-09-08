b4703749ef0a10ff21dbd7b094229751090c69a9
RSL459 <2264768831@qq.com>
2026-09-08 23:23:08 +0800

    style(router): format scorer re-exports

--- data-plane/frontend/src/router/src/algorithm/mod.rs
+++ data-plane/frontend/src/router/src/algorithm/mod.rs
@@ -30,5 +30,6 @@ pub mod scorer;
 pub use filter::{AllowAllFilter, RouteFilter};
 pub use picker::{MaxPicker, RoundRobinPicker, RoutePicker};
 pub use scorer::{
-    KvLeastLoadedScorer, LeastLoadedScorer, QueueDepthScorer, RouteScorer, RunningRequestScorer, UniformScorer,
+    KvLeastLoadedScorer, LeastLoadedScorer, QueueDepthScorer, RouteScorer, RunningRequestScorer,
+    UniformScorer,
 };
