486075031ae6e44f4c1e1642c2a7abf05525fa73
RSL459 <2264768831@qq.com>
2026-09-08 22:29:38 +0800

    chore(router): align running-request scorer source headers

--- data-plane/frontend/src/router/src/algorithm/scorer/mod.rs
+++ data-plane/frontend/src/router/src/algorithm/scorer/mod.rs
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright contributors to the Foretoken project
-// SPDX-FileCopyrightText: Copyright 2025 The Kubernetes Authors
 
 //! Candidate scoring and Scorer implementations.
 
