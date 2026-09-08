d972db83c127224babcc808ba4b038a664bc509c
RSL459 <2264768831@qq.com>
2026-09-09 00:47:24 +0800

    refactor(router): simplify algorithm re-exports

--- data-plane/frontend/src/router/src/algorithm/mod.rs
+++ data-plane/frontend/src/router/src/algorithm/mod.rs
@@ -27,9 +27,6 @@ pub mod filter;
 pub mod picker;
 pub mod scorer;
 
-pub use filter::{AllowAllFilter, RouteFilter};
-pub use picker::{MaxPicker, RoundRobinPicker, RoutePicker};
-pub use scorer::{
-    KvCacheUtilizationScorer, KvLeastLoadedScorer, LeastLoadedScorer, QueueDepthScorer,
-    RouteScorer, UniformScorer,
-};
+pub use filter::*;
+pub use picker::*;
+pub use scorer::*;
